932f4ed2654501b29a1e76166fff9697e262e546
Agha-Javed <javed.ahmed.pathan@gmail.com>
2016-01-28 17:22:05 +0800

    modified classes.

--- app/com/aztask/util/Util.java
+++ app/com/aztask/util/Util.java
@@ -1,6 +1,31 @@
 package com.aztask.util;
 
+import java.util.List;
+import com.aztask.vo.NearByDevice;
+
 public class Util {
 	
+	
+	public static String getWhereCluase(List<NearByDevice> nearByUsers){
+		StringBuffer whereCluase=new StringBuffer("(");
+		for(NearByDevice nearByUser: nearByUsers){
+			whereCluase.append("'"+nearByUser.getDevice_id()+"',");
+		}
+		whereCluase.deleteCharAt(whereCluase.length()-1);
+		whereCluase.append(")");
+		return whereCluase.toString();
+	}
+	
+	public static String getLikeClause(String columnName,String categories){
+		
+		StringBuffer likeCluase=new StringBuffer();
+		for(String taskCategory : categories.split(";")){
+			likeCluase.append(" "+columnName+" like '%"+taskCategory+"%' or");
+		}
+		likeCluase.delete(likeCluase.length()-2, likeCluase.length());
+		return likeCluase.toString();
+		
+	}
+
 
 }
